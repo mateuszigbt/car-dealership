8662cef26fa0c92db0c410ff9a76fe3a6ed65c4d
mateuszigbt <mniebora@gmail.com>
2024-12-06 17:05:51 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Product } from "../domain/product";
+import { ProductService } from "../service/productservice";
+import { ImportModules } from "./imports";
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ImportModules],
+  providers: [ProductService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'car-dealership';
+
+  cities: City[] | undefined;
+  selectedCity: City | undefined;
+
+  products: Product[] = [];
+  responsiveOptions: any[] | undefined;
+
+  constructor() { }
+
+  ngOnInit() {
+    
+
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
 }
---
+++ src/app/imports.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { ProductService } from '../service/productservice';
+
+@NgModule({
+    imports: [
+        RouterOutlet, 
+        ButtonModule, 
+        DropdownModule, 
+        FormsModule, 
+        ReactiveFormsModule, 
+        CarouselModule, 
+        TagModule
+    ],
+    exports: [
+        RouterOutlet, 
+        ButtonModule, 
+        DropdownModule, 
+        FormsModule, 
+        ReactiveFormsModule, 
+        CarouselModule, 
+        TagModule
+    ],
+    providers: [ProductService]
+})
+export class ImportModules {}
